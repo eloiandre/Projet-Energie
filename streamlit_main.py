90bb6dfa377cfc6365eb24a5d6ef3c7755d36bd0
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-08-31 13:01:55 +0200

    Create streamlit_main.py

---
+++ streamlit_main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import gdown
+import plotly.graph_objects as go
+import plotly.express as px
+import os
+from PIL import Image
+
+
+st.title("Projet Energie")
+st.sidebar.title("Sommaire")
+pages=["Definition du Projet","Exploration", "DataVizualization", "Modélisation"]
+page=st.sidebar.radio("Aller vers", pages)
