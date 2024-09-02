cf37578f68810997adae0ec135e657fd9b2fc4ab
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-02 22:00:19 +0200

    a

--- streamlit_main.py
+++ streamlit_main.py
@@ -20,9 +20,14 @@ def import_files():
     gdown.download(url_temperature_csv, output_temperature_csv, quiet=False)
     temperature = pd.read_csv(output_temperature_csv)
 
+    url_new_file = "https://drive.google.com/uc?export=download&id=1dTO2ME4O5OuDKbfshDBqfiovLo8-s0O-"
+    output_new_file = "new_file.csv"
+    gdown.download(url_new_file, output_new_file, quiet=False)
+    new_data = pd.read_csv(output_new_file)
+
     url_geojson = "https://raw.githubusercontent.com/eloiandre/Projet-Energie/main/regions.geojson"
     geojson = gpd.read_file(url_geojson)
-    return(df,geojson,temperature)
+    return(df,geojson,temperature,new_data)
 
 def show_definition():
     st.write('## Definition du projet :')
@@ -480,8 +485,9 @@ def conso_temp():
 def show_model():
     st.write('## Model :')
 def main():
-    st.title("Projet Energie1")
+    st.title("Projet Energie12")
     st.sidebar.title("Sommaire")
+    st.table(initial.head())
     pages=["Definition du Projet","Exploration", "DataVizualization", "Modélisation"]
     page=st.sidebar.radio("Aller vers", pages)
     if page ==pages[0]:
@@ -492,5 +498,5 @@ def main():
         show_data_viz()
     if page==pages[3]:
         show_model()
-df,geojson,temperature=import_files()
+df,geojson,temperature,initial=import_files()
 main()
