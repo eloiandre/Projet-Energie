28ef9304a34e81a9d9c47a663ecc8d13e93b62aa
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-02 10:13:50 +0200

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -7,15 +7,6 @@ import plotly.express as px
 import os
 from PIL import Image
 
-def import_files():
-    # Use the direct download link from Google Drive
-    #url_csv = "https://drive.google.com/uc?export=download&id=1--2Tsgm3InoAqYkzKlvq0ylJ8JcBmjNU"
-    #output_csv = "data.csv"
-
-    #gdown.download(url_csv, output_csv, quiet=False)
-    #df = pd.read_csv(output_csv)
-    st.write("ok")
-
 
 import streamlit as st
 st.cache_data.clear()
