66e2b7a6f1c304e19b4c782eff51bfd88015cd81
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-02 11:02:18 +0200

    a

--- streamlit_main.py
+++ streamlit_main.py
@@ -24,8 +24,49 @@ def show_exploration():
     st.dataframe(df.head(10))
     st.write(f"Dimensions du DataFrame: {df.shape}")
     st.dataframe(df.describe())
+#fonction pour la page Définition
+def show_definition():
+    st.write('## Definition du projet :')
+    st.write('« Constater le phasage entre la consommation et la production énergétique, au niveau national et au niveau régional (risque de black out notamment) »')
+    st.info("Depuis 2022 en France, on parle de plus en plus de sécurité d'approvisionnement électrique. \
+    Le parc nucléaire est de moins en moins renouvelé; laissant de la place au développement du parc à énergies renouvelables.")
+    
+    image_compo = Image.open('composition_nucleaire.png')
+    st.image(image_compo)
+    st .write('Depuis 2020, le poids du nucléaire est de 66,5% de la production globale. Les énergies renouvelables quant à elles comptent pour 25.1%.')
+
+    piechart = Image.open('piechart.png')
+    st.image(piechart)
+    st.write("Les énergies renouvelables prennent de plus en plus de place. Une telle évolution des types d'énergies pose le risque de 'blackout', \
+              à savoir des coupures d'approvisionnement sur tout ou partie du territoire. \
+              Aujourd'hui, il est très difficile de stocker l'énergie. Il faut alors s'assurer que la production subvienne bien à la demande à chaque instant.")
+    
+    prod_type = Image.open('production_par_type.png')
+    st.image(prod_type)
+    st.write("Ce projet porte sur l'analyse de données extraites par une application nommée eCO²mix [link] https://www.rte-france.com/eco2mix. \
+             Le défi est de pouvoir entraîner un modèle capable de prédire la consommation en éléctricité, par demie heure et par région.")
+    st.markdown("""
+    <style>
+            .right-align {
+			    text-align: right;
+		    }
+	</style>
+	""", unsafe_allow_html=True)
+
+    st.markdown(
+    """
+    Plusieurs aspects seront observés:
+    - Analyse au niveau régional pour en déduire une prévision de consommation
+    - Analyse par filière de production : énergie nucléaire / renouvelable
+    - Focus sur les énergies renouvelables (où sont- elles implantées ?)
+    """
+    )
+    st.markdown('<p class="right-align">Membres du groupe: Eloi Andre, Pierre Valmont, Siyamala Rollot, Léa Henry-Beaupied,  </p>', unsafe_allow_html=True)
+    st.markdown('<p class="right-align">Date: Septembre 2024</p>', unsafe_allow_html=True)
+
+
 df,geojson=import_files()
-st.title("Projet aaEnergie")
+st.title("Projet Energie")
 st.sidebar.title("Sommaire")
 pages=["Definition du Projet","Exploration", "DataVizualization", "Modélisation"]
 page=st.sidebar.radio("Aller vers", pages)
