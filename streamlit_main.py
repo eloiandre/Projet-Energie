59aabdb7a258e511f725fc4371fca05c72ba8359
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-02 09:42:36 +0200

    a

--- streamlit_main.py
+++ streamlit_main.py
@@ -8,6 +8,19 @@ import os
 from PIL import Image
 
 
+def import_files():
+    url_csv = "https://drive.google.com/uc?export=download&id=1--2Tsgm3InoAqYkzKlvq0ylJ8JcBmjNU"
+    output_csv = "data.csv"
+    gdown.download(url_csv, "data.csv", quiet=False)
+    df = pd.read_csv(output_csv)
+
+    url_geojson = "https://raw.githubusercontent.com/eloiandre/test_stream/main/regions.geojson"
+    geojson = gpd.read_file(url_geojson)
+    return(df,geojson)
+
+
+
+import files
 st.title("Heeey")
 st.sidebar.title("Sommaire")
 pages=["Definition du Projet","Exploration", "DataVizualization", "Modélisation"]
