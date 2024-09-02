eb5e530dc7f440ddcebcbc2cc5df653e79ce454d
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-02 10:00:16 +0200

    a

--- streamlit_main.py
+++ streamlit_main.py
@@ -10,7 +10,7 @@ from PIL import Image
 def import_files():
     # Use the direct download link from Google Drive
     #https://drive.google.com/file/d/1uW-GP29vVrHz-Whh5LYCaCDckuqnCbGj/view?usp=drive_link
-    url_csv = "https://drive.google.com/file/d/1uW-GP29vVrHz-Whh5LYCaCDckuqnCbGj/view?usp=drive_link"
+    url_csv = "https://drive.google.com/uc?export=download&id=1uW-GP29vVrHz-Whh5LYCaCDckuqnCbGj"
     output_csv = "data.csv"
     gdown.download(url_csv, output_csv, quiet=False)
     df = pd.read_csv(output_csv)
