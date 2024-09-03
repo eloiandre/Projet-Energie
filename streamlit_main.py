06a604e1f7920952facc7f46cf4e33832c11bf13
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-03 10:35:43 +0200

    a

--- streamlit_main.py
+++ streamlit_main.py
@@ -517,7 +517,7 @@ def tableaux_modeles():
     st.dataframe(df_results)
 def feature_importance():
     fig = go.Figure()
-    df_feature=df_features.sort_values(by='Importance',ascending=True)
+    df_features=df_features.sort_values(by='Importance',ascending=False)
     fig.add_trace(go.Bar(
         y=df_features.Feature,
         x=df_features.Importance,
@@ -549,7 +549,6 @@ def show_model():
     # Données sous forme de dictionnaire
     st.write('## Feature Importance :')
     feature_importance()
-    st.write(df_features.head())
 
 
 def main():
