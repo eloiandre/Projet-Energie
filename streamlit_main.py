62f5a50496a8ab3a840f93dcf215c4bde935c1de
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-03 10:01:22 +0200

    a

--- streamlit_main.py
+++ streamlit_main.py
@@ -516,6 +516,8 @@ def show_model():
     st.write('# Objectif : Prédire la consommation par région')
     st.write('Tableau comparatif de nos modéles :')
     tableaux_modeles()
+    st.write('Les modéles Random Forest , XGboost et Decision Tree ont les meilleures performances. Mais le score trop élevé des deux premiers\
+             ressembe à du suraprentissage')
     #courbe d'aprentissage
     courbe_apprentissage=Image.open('Courbe_apprentissage.jpg')
     st.image(courbe_apprentissage)
