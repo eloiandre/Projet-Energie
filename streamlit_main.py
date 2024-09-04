f94ea2611a54f3952a6e719394fd17abddcdc0b8
eloiandre <155251821+eloiandre@users.noreply.github.com>
2024-09-04 15:29:52 +0200

    a

--- streamlit_main.py
+++ streamlit_main.py
@@ -613,10 +613,10 @@ def plot_comparison(y_test, y_pred, num_values=50):
 def create_result_df(y_pred,y_test):
     st.write('### create result')
     y_pred=pd.Series(y_pred)
-    #y_test = pd.Series(y_test, index=y_pred.index, name='y_test')
+    y_test = pd.Series(y_test, index=y_pred.index, name='y_test')
     #df_result=pd.concat([y_test,y_pred],axis=1)
-    st.write(y_pred.head())
-    #st.write(y_test.head())
+    #st.write(y_pred.head())
+    st.write(y_test.head())
 def show_model():
     st.write('### Modéles :')
     st.write('## Objectif : Prédire la consommation par région')
